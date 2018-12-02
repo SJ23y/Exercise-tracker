af7976da3c9b58ed6f3404e34cf1bc1b00b8a306
Glitch (hello-express) <none>
2018-12-02 11:05:23 +0000

    🛩🚥 Checkpoint ./server.js:997454/417 ./package.json:997454/1283

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
-require('mongodb').MongoClient;
+const mongo = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+app.use('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-
+mongo.connect(process.env.MONGO_URI, function(err,db) {
+    if (err) {console.log('Database error: ' + err);}
+    else {
+      console.log('Successful database connection');
+    }
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
